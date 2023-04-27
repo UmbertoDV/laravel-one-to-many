c382af78d113823bba479d309255445dfee0ad1c
UmbertoDV <biccoumby2002@hotmail.com>
2023-04-27 16:40:56 +0200

    style show

--- resources/views/admin/cards/show.blade.php
+++ resources/views/admin/cards/show.blade.php
@@ -9,8 +9,17 @@
 
   <div class="card">
     <div class="card-body">
-      <img src="{{ $card->getImageUri() }}" alt="" width="250" class="float-start me-3 mb-1">
-      <p>{{ $card->text }}</p>
+      <p>
+        <strong>Categoria:</strong>
+        <span class="badge rounded-pill" style="background-color: {{ $card->category?->color }}">{{ $card->category?->label }}</span>
+      </p>
+      <div>
+        <strong>Testo:</strong>
+        <p>{{ $card->text }}</p>
+      </div>
+      <div>
+        <img src="{{ $card->getImageUri() }}" alt="" width="250" class="me-3 mb-1">
+      </div>
     </div>
   </div>
 </section>
